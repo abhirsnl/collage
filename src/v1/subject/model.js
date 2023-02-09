34c23ad60f545ed68361954615135626aea53f78
abhirsnl <react.nodejs16@gmail.com>
2023-02-09 17:11:11 +0530

    for subject schema

---
+++ src/v1/subject/model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+
+let subjectSchema=new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        default:null,
+        required:true
+    }
+})
+let 
