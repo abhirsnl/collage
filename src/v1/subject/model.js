c4ebbeed27f012eb7320bb238d2924e963d0bd71
abhirsnl <react.nodejs16@gmail.com>
2023-02-09 17:27:08 +0530

    merge

--- src/v1/subject/model.js
+++ src/v1/subject/model.js
@@ -1,2 +1,11 @@
 const mongoose = require('mongoose')
 
+
+let subjectSchema=new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        default:null,
+        required:true
+    }
+})
+let 
